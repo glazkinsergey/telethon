35fb7443d823a776f1fb8ec1750f96b10f400518
glazkinsergey <35304096+glazkinsergey@users.noreply.github.com>
2022-04-02 15:40:36 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,8 @@
+from os import environ
+from dotenv import load_dotenv
+
+# Загрузка значений переменных окружения
+load_dotenv()
+
+API_ID = environ.get('API_ID')
+API_HASH = environ.get('API_HASH')
